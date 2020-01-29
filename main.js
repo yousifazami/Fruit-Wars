97e16f670f2823e8b919b716639363ca19d9a55f
ajp54 <58841366+ajp54@users.noreply.github.com>
2020-01-29 13:56:54 -0800

    Add files via upload
    Added terrain generation

--- main.js
+++ main.js
@@ -176,6 +176,62 @@ rocket.prototype.draw = function () {
 		Entity.prototype.draw.call(this);
 }
 
+function Terrain(game) {
+    Entity.call(this, game, 0, 200);
+    this.radius = 100;
+    let coordinates = [];
+}
+
+Terrain.prototype = new Entity();
+Terrain.prototype.constructor = Terrain;
+
+Terrain.generate = function (points, groundLevel, yVariance) {
+	let height = 700;
+	let width = 800;
+    let x = 0;
+    let y = groundLevel;
+    let xVariance = height/points * 2;
+    let oldx = 0;
+    coordinates = [];
+    coordinates.push([0, height]);
+    for(let i = 0; i < points; i++) {
+        coordinates.push([x, y]);
+        console.log("x: " + coordinates[i][0] + ", y: " + coordinates[i][1]);
+        oldx = x;
+        x += Math.random() * xVariance;
+        var rand = Math.random() * 100;
+        console.log("random: " + rand);
+        y = Math.floor(rand) % 2 ? y +  Math.random() * yVariance : y -  Math.random() * xVariance;
+        if(y > height)
+            y = y % height + groundLevel;
+        else if(y < height/2)
+            y += 200; // pick a good number for the canvas size
+        
+        if(x-oldx < 50)
+            x += width/points;
+    }
+    coordinates.push([width, groundLevel]);
+    coordinates.push([width, height]);
+    
+return coordinates;
+
+} 
+
+Terrain.prototype.update = function () {
+
+}
+
+Terrain.prototype.draw = function (ctx) {
+    ctx.fillStyle = "Peru";
+
+    ctx.beginPath();
+    ctx.moveTo(0, 700);
+    for(let i = 0; i < this.coordinates.length; i++) {
+        ctx.lineTo(this.coordinates[i][0], this.coordinates[i][1]);
+    }
+    ctx.fill();
+}
+
 AM.queueDownload("./img/explosion/expl_07_0000.png");
 AM.queueDownload("./img/explosion/expl_07_0001.png");
 AM.queueDownload("./img/explosion/expl_07_0002.png");
@@ -328,7 +384,10 @@ AM.downloadAll(function () {
 	ninjaRun.push(AM.getAsset("./img/ninja/Run__007.png"));
 	ninjaRun.push(AM.getAsset("./img/ninja/Run__008.png"));
 	ninjaRun.push(AM.getAsset("./img/ninja/Run__009.png"));
-    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background0.png")));
+	var ground = new Terrain(gameEngine);
+	ground.coordinates = Terrain.generate(50, 500, 50);
+	gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background0.png")));
+	gameEngine.addEntity(ground);
 	gameEngine.addEntity(new ninja(gameEngine, ninjaRun));
 	gameEngine.addEntity(new rocket(gameEngine, rocketArr));
 	gameEngine.addEntity(new tninja(gameEngine, tninjaArr));
